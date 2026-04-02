4663a4f4c9ea185b6af5f7f61013e57cfba0e2fe
eg0r112 <Vedromakaron@gmail.com>
2026-04-02 18:07:56 +0300

    Create UpdateDocumentRequest.cs

---
+++ Day2Vizov3.0/Models/UpdateDocumentRequest.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Day2Vizov3._0.Models;
+
+public class UpdateDocumentRequest
+{
+    [Required]
+    [MinLength(1)]
+    [MaxLength(100)]
+    public string Title { get; set; } = string.Empty;
+    
+    [MaxLength(5000)]
+    public string Content { get; set; } = string.Empty;
+    
+    public bool IsPublic { get; set; } = false;
+}
