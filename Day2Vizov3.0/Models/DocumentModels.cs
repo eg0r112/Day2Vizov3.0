a9e472e44078a7b64b5f271fd59828eeb950f337
eg0r112 <Vedromakaron@gmail.com>
2026-04-02 17:32:52 +0300

    load

---
+++ Day2Vizov3.0/Models/DocumentModels.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Day2Vizov3._0.Models;
+
+public class CreateDocumentRequest
+{
+    [Required]
+    [MinLength(1)]
+    [MaxLength(100)]
+    public string Title { get; set; } = string.Empty;
+    
+    [MaxLength(5000)]
+    public string Content { get; set; } = string.Empty;
+    
+    public bool IsPublic { get; set; } = false;
+}
+
+public class DocumentResponse
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public bool IsPublic { get; set; }
+    public string OwnerName { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
