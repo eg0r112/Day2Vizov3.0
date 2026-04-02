89a2330d4bc17de21a05573f458096f75d856e22
eg0r112 <Vedromakaron@gmail.com>
2026-04-02 18:12:20 +0300

    Update DocumentModels.cs

--- Day2Vizov3.0/Models/DocumentModels.cs
+++ Day2Vizov3.0/Models/DocumentModels.cs
@@ -15,6 +15,19 @@ public class CreateDocumentRequest
     public bool IsPublic { get; set; } = false;
 }
 
+public class UpdateDocumentRequest
+{
+    [Required]
+    [MinLength(1)]
+    [MaxLength(100)]
+    public string Title { get; set; } = string.Empty;
+    
+    [MaxLength(5000)]
+    public string Content { get; set; } = string.Empty;
+    
+    public bool IsPublic { get; set; } = false;
+}
+
 public class DocumentResponse
 {
     public int Id { get; set; }
